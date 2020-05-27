d4cf5943ca58e183ba62892d126d10a87788f7e5
Tara Patel <t.jeanne.patel@gmail.com>
2020-05-27 16:17:56 -0500

    added some variable types

--- Playgrounds/Variables.playground/Contents.swift
+++ Playgrounds/Variables.playground/Contents.swift
@@ -2,12 +2,29 @@ import UIKit
 
 //Variables and Constants
 
-var userName = "Taco Bell"
+//String
+var userName = "Taco"
+userName.append(" Bell")
+userName.lowercased()
 print(userName)
 var userSurname = "Pizza Hut"
 
 userName = "KFC"
 print(userName)
 
+//integersm doubles, floats
 let userAge = 50
+let myNumber = 10
+
+print(userAge / myNumber)
 print(userAge)
+
+let userAgeD = 50.0
+let myNumberD = 3.0
+
+print(userAgeD / myNumberD)
+
+//Booleans
+
+var myBoolean = false
+print(myBoolean == false)
