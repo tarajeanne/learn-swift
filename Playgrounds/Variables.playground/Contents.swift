a805f311234d8d8e9113846713e37a0a3aad2e76
Tara Patel <t.jeanne.patel@gmail.com>
2020-05-27 16:13:25 -0500

    added Playgrounds directory

---
+++ Playgrounds/Variables.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Variables and Constants
+
+var userName = "Taco Bell"
+print(userName)
+var userSurname = "Pizza Hut"
+
+userName = "KFC"
+print(userName)
+
+let userAge = 50
+print(userAge)
