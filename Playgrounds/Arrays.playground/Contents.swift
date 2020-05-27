e4f7db9b126b67c64a259bfd23b5abea63f1aafa
Tara Patel <t.jeanne.patel@gmail.com>
2020-05-27 16:51:17 -0500

    added sets

--- Playgrounds/Arrays.playground/Contents.swift
+++ Playgrounds/Arrays.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var str = "Hello, playground"
+var myFavoriteMovies = ["Taco", "Bell", "KFC"] as [Any]
+myFavoriteMovies.append(50)
+
+var myStringArray = ["Chicken", "Beef", "Pork"]
+
+myStringArray.sort()
+
+//Sets
+//Myst have unique values, unordered
+
+var mySet : Set = [1, 2, 3, 4, 5, 6]
+
+print(mySet)
+print(mySet.first)
+
+var myInternetArray = [1, 2, 3, 4, 2, 3, 4, 5]
+var myInternetSet = Set(myInternetArray)
+print(myInternetArray, myInternetSet)
+
+var newSet = mySet.union(myInternetSet)
